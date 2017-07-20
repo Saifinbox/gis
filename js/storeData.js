601b36e166836c348b1990597249325e40435030
saifinbox <muhammad.saif@inboxbiz.com>
2017-07-20 12:01:29 -0700

    push

---
+++ js/storeData.js
@@ -0,0 +1,65 @@
+var store_contents=[];
+
+store_contents[0]= '<div id="iw-container">' +
+                  '<div class="iw-title"> '+ jsondata[0].NameStore +'</div>' +
+                  '<div class="iw-content">' +
+                  '</br>' +
+                    '<div class="iw-subTitle">KPK</div>' +
+                    '<style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style>' +
+                    
+     '<table><tr><th style=col width=100>Total Dataset</th><th>Organizations</th><th style=col width=100>Latitude</th><th style=col width=100>Longitude</th></tr><tr>\
+    <td style=col align=center>10</td><td style=col align=center>3</td>\
+    <td style=col align=center>24.8460558</td>\
+    <td style=col align=center>67.0231379</td>\
+  </tr>\
+</table>' 
+                '</div>';
+
+store_contents[1]= '<div id="iw-container">' +
+                  '<div class="iw-title"> '+ jsondata[1].NameStore +'</div>' +
+                  '<div class="iw-content">' +
+                  '</br>' +
+                    '<div class="iw-subTitle">Islamabad</div>' +
+                    '<style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style>' +
+                    
+     '<table><tr><th style=col width=100>Total Dataset</th><th>Organizations</th><th style=col width=100>Latitude</th><th style=col width=100>Longitude</th></tr><tr>\
+    <td style=col align=center>8</td>\
+    <td style=col align=center>3</td>\
+    <td style=col align=center>24.846076</td>\
+    <td style=col align=center>67.023275</td>\
+  </tr>\
+</table>'
+                '</div>';
+
+
+store_contents[2]= '<div id="iw-container">' +
+                  '<div class="iw-title"> '+ jsondata[2].NameStore +'</div>' +
+                  '<div class="iw-content">' +
+                  '</br>' +
+                    '<div class="iw-subTitle">Sindh</div>' +
+                    '<style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style>' +
+                    
+     '<table><tr><th style=col width=100>Total Dataset</th><th>Organizations</th><th style=col width=100>Latitude</th><th style=col width=100>Longitude</th></tr><tr>\
+    <td style=col align=center>13</td>\
+    <td style=col align=center>4</td>\
+    <td style=col align=center>24.8460389</td>\
+    <td style=col align=center>67.0229773</td>\
+  </tr>\
+</table>'
+                '</div>';
+
+                store_contents[3] = '<div id="iw-container">' +
+                                  '<div class="iw-title"> ' + jsondata[3].NameStore + '</div>' +
+                                  '<div class="iw-content">' +
+                                  '</br>' +
+                                    '<div class="iw-subTitle">Punjab</div>' +
+                                    '<style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style>' +
+
+                     '<table><tr><th style=col width=100>Total Dataset</th><th>Organizations</th><th style=col width=100>Latitude</th><th style=col width=100>Longitude</th></tr><tr>\
+    <td style=col align=center>5</td>\
+    <td style=col align=center>4</td>\
+    <td style=col align=center>24.8460389</td>\
+    <td style=col align=center>67.0229773</td>\
+  </tr>\
+</table>'
+                '</div>';
